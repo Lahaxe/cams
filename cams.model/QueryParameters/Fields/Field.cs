48280a5f2c01339cdbe2464309232d34a7557785
Lahaxe <lahaxe@unistra.fr>
2018-02-24 17:40:30 +0100

    Add fielding parameters

---
+++ cams.model/QueryParameters/Fields/Field.cs
@@ -0,0 +1,18 @@
+﻿namespace cams.model.QueryParameters.Fields
+{
+    /// <summary>
+    /// Defines a field option.
+    /// </summary>
+    public class Field
+    {
+        /// <summary>
+        /// Fielding attribute.
+        /// </summary>
+        public string Attribute { get; set; }
+
+        /// <summary>
+        /// Fielding visibility.
+        /// </summary>
+        public FieldingVisibility Visibility { get; set; }
+    }
+}
---
+++ cams.model/QueryParameters/Fields/FieldingParametersBase.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace cams.model.QueryParameters.Fields
+{
+    public class FieldingParametersBase : Collection<Dictionary<string, FieldingVisibility>>
+    {
+    }
+}
