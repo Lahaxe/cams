f18551c36412e92feb2b2574bc882f3b4e7c5bfd
Lahaxe <lahaxe@unistra.fr>
2018-02-24 17:28:43 +0100

    Add filtering parameters

---
+++ cams.model/QueryParameters/Filters/FilterOperator.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace cams.model.QueryParameters.Filters
+{
+    /// <summary>
+    /// Defines the filter operators.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum FilterOperator
+    {
+        /// <summary>
+        /// Logical operator AND.
+        /// </summary>
+        [EnumMember(Value = "and")]
+        And,
+
+        /// <summary>
+        /// Logical operator OR.
+        /// </summary>
+        [EnumMember(Value = "or")]
+        Or,
+
+        /// <summary>
+        /// Operator Equal.
+        /// </summary>
+        [EnumMember(Value = "eq")]
+        Equal,
+
+        /// <summary>
+        /// Operator Not equal.
+        /// </summary>
+        [EnumMember(Value = "ne")]
+        NotEqual,
+
+        /// <summary>
+        /// Operator In.
+        /// </summary>
+        [EnumMember(Value = "in")]
+        In,
+
+        /// <summary>
+        /// Operator Greater than.
+        /// </summary>
+        [EnumMember(Value = "gt")]
+        GreaterThan,
+
+        /// <summary>
+        /// Operator Greater or equal.
+        /// </summary>
+        [EnumMember(Value = "ge")]
+        GreaterOrEqual,
+
+        /// <summary>
+        /// Operator Less than.
+        /// </summary>
+        [EnumMember(Value = "lt")]
+        LessThan,
+
+        /// <summary>
+        /// Operator Less or equal.
+        /// </summary>
+        [EnumMember(Value = "le")]
+        LessOrEqual
+    }
+}
